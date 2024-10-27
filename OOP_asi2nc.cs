a660364359b626b1c7726a5c0161c594c2ddc076
HusseinAnwarZ <rtyfgh456789iop@gmail.com>
2024-10-27 06:34:23 +0300

    Initial commit

---
+++ OOP_asi2nc.cs
@@ -0,0 +1,45 @@
+﻿//OOP_asi2nc
+car c = new car("BMW","2006");
+c.print();
+
+truck t = new truck("mercedes", "2025");
+t.print();
+Console.ReadLine();
+
+class veachle
+{
+  protected   string Brand;
+}
+abstract class motorveachle : veachle
+{
+   protected string model;
+    
+    public abstract  void print ();
+    
+}
+class car:motorveachle
+{
+    public car(string Brand,string model)
+    {
+        this.Brand = Brand;
+        this.model = model;
+    }
+    public override void print()
+    {
+        Console.WriteLine("Brand  : "+ Brand );
+        Console.WriteLine("model  : " + model);
+    }
+}
+class truck : motorveachle
+{
+    public truck(string Brand, string model)
+    {
+        this.Brand = Brand;
+        this.model = model;
+    }
+    public override void print()
+    {
+        Console.WriteLine("Brand  : " + Brand);
+        Console.WriteLine("model  : " + model);
+    }
+}
